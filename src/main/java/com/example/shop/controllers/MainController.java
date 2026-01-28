15a9707872beeee6fb88ff4dcb7617134bd776cc
Yan Lagun <exm230600@gmail.com>
2026-01-28 20:24:24 +0100

    Add 2 html fils and 1 main controller

---
+++ src/main/java/com/example/shop/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.example.shop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String index() {
+
+        return "index";
+    }
+
+    @GetMapping("/about-us")
+    public String about(@RequestParam(name = "userName", required = false, defaultValue = "World")
+                            String userName, Model model) {
+        model.addAttribute("name", userName);
+        return "about-us";
+    }
+
+}
